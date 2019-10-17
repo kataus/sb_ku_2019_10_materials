b950862f7e332e6237a0afa111f9ea0d5ab84fde
kataus <kataus@gmail.com>
2019-10-17 09:58:51 +0300

    Материалы первой лекции

---
+++ L01/Maven_example/maven-sample-child/src/test/java/ru/sbrf/java_course/vk/AppTest.java
@@ -0,0 +1,20 @@
+// package ru.sbrf.java_course.vk;
+
+// import static org.junit.Assert.assertTrue;
+
+// import org.junit.Test;
+
+// /**
+//  * Unit test for simple App.
+//  */
+// public class AppTest 
+// {
+//     /**
+//      * Rigorous Test :-)
+//      */
+//     @Test
+//     public void shouldAnswerWithTrue()
+//     {
+//         assertTrue( true );
+//     }
+// }
