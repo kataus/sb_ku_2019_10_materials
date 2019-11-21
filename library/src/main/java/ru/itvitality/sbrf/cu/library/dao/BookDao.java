d8c78f47385de785c8f0890e7525f6009c88f561
kataus <kataus@gmail.com>
2019-11-21 20:33:07 +0300

    Процесс занятия  (21/11/2019)

---
+++ library/src/main/java/ru/itvitality/sbrf/cu/library/dao/BookDao.java
@@ -0,0 +1,17 @@
+package ru.itvitality.sbrf.cu.library.dao;
+
+import ru.itvitality.sbrf.cu.library.entities.Book;
+
+import java.util.List;
+
+public interface BookDao {
+    void add( Book book );
+
+    List<Book> list();
+
+    Book get( Long id );
+
+    void remove( Long id );
+
+    List<Book> getBooksOnHolder(Long holderId);
+}
---
+++ library/src/main/java/ru/itvitality/sbrf/cu/library/entities/Placement.java
@@ -0,0 +1,4 @@
+package ru.itvitality.sbrf.cu.library.entities;
+
+public class Placement {
+}
