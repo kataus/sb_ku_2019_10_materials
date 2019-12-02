efe3738e80acbe0487e363453592c6dafbef3e4d
kataus <kataus@gmail.com>
2019-12-02 21:11:08 +0300

    Пример на занятии до аспекта 02/12/2019 (14-е занятие)

--- L14_spring_xml/src/main/java/ru/itvitality/sbrf/cu/l14xml/Main.java
+++ L14_spring_xml/src/main/java/ru/itvitality/sbrf/cu/l14xml/Main.java
@@ -2,12 +2,16 @@ package ru.itvitality.sbrf.cu.l14xml;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.itvitality.sbrf.cu.l14xml.domain.Person;
+import ru.itvitality.sbrf.cu.l14xml.service.PersonService;
+import ru.itvitality.sbrf.cu.l14xml.service.PersonServiceImpl;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        Person ivan = null;
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( "/spring-context.xml" );
+        PersonService personService = context.getBean( PersonServiceImpl.class );
+        Person ivan = personService.getByName( "ivan" );
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
     }
 }
