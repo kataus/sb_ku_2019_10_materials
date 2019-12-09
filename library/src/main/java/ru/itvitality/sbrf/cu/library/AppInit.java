19db5a336292ebe2014730993e8fc165d2350e78
kataus <kataus@gmail.com>
2019-12-09 20:00:42 +0300

    добавление AppInit

---
+++ library/src/main/java/ru/itvitality/sbrf/cu/library/AppInit.java
@@ -0,0 +1,30 @@
+package ru.itvitality.sbrf.cu.library;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    // Этот метод должен содержать конфигурации которые инициализируют Beans
+    // для инициализации бинов у нас использовалась аннотация @Bean
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    // Тут добавляем конфигурацию, в которой инициализируем ViewResolver
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
+
