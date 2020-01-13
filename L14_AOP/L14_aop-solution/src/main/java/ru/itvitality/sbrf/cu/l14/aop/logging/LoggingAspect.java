5fd81e4657d69977b6c68cbf180e926dd5b23109
kataus <kataus@gmail.com>
2020-01-13 15:49:23 +0300

    Эксперименты с аспектами

--- L14_AOP/L14_aop-solution/src/main/java/ru/itvitality/sbrf/cu/l14/aop/logging/LoggingAspect.java
+++ L14_AOP/L14_aop-solution/src/main/java/ru/itvitality/sbrf/cu/l14/aop/logging/LoggingAspect.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-
     @Before("execution(* ru.itvitality.sbrf.cu.l14.aop.dao.PersonDaoSimple.*(..))")
     public void logBefore(JoinPoint joinPoint) {
         System.out.println("Вызов метода : " + joinPoint.getSignature().getName());
